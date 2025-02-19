6f8d1c3656bba2cd613b02fc65e6807985f52f37
mtkuwav <math.chaulab@gmail.com>
2025-02-19 20:12:42 +0100

    chore(User): added missing docstrings

--- app/src/controllers/User.php
+++ app/src/controllers/User.php
@@ -17,22 +17,50 @@ class User extends Controller {
         parent::__construct($param);
     }
 
+    /**
+     * Delete a user by ID.
+     *
+     * @return array The deletion result
+     * @throws HttpException if deletion fails
+     * @author Rémis Rubis
+     */
     #[Route("DELETE", "/users/:id", middlewares: [AuthMiddleware::class], allowedRoles:['admin'])]
     public function deleteUser() {
         return $this->user->delete(intval($this->params['id']));
     }
 
+    /**
+     * Get a specific user by ID.
+     *
+     * @return array The user data
+     * @throws HttpException if user not found
+     * @author Rémis Rubis
+     */
     #[Route("GET", "/users/:id", middlewares: [AuthMiddleware::class], allowedRoles:['admin'])] 
     public function getUser() {
         return $this->user->get(intval($this->params['id']));
     }
 
+    /**
+     * Get all users with optional limit.
+     * 
+     * @param int|null $limit Optional parameter to limit the number of returned users
+     * @return array Array of user records
+     * @author Rémis Rubis
+     */
     #[Route("GET", "/users", middlewares: [AuthMiddleware::class], allowedRoles:['admin'])]
     public function getUsers() {
             $limit = isset($this->params['limit']) ? intval($this->params['limit']) : null;
             return $this->user->getAll($limit);
     }
 
+    /**
+     * Update user information.
+     *
+     * @throws HttpException if the request body is empty or contains no valid fields
+     * @return array The updated user data
+     * @author Rémis Rubis, Mathieu Chauvet
+     */
     #[Route("PATCH", "/users/:id", middlewares: [AuthMiddleware::class], allowedRoles:['admin'])]
     public function updateUser() {
         try {
@@ -59,15 +87,22 @@ class User extends Controller {
         }
     }
 
+    /**
+     * Promotes a user to admin role.
+     *
+     * @throws HttpException if promotion fails
+     * @return array The updated user data after promotion
+     * @author Mathieu Chauvet
+     */
     #[Route("POST", "/users/:id/promote", middlewares: [AuthMiddleware::class], allowedRoles: ['admin'])]
     public function promoteUser() {
         try {
-                $id = intval($this->params['id']);
-                return $this->user->promoteToAdmin($id);
+            $id = intval($this->params['id']);
+            return $this->user->promoteToAdmin($id);
         } catch (HttpException $e) {
-                throw $e;
+            throw $e;
         } catch (\Exception $e) {
-                throw new HttpException($e->getMessage(), 500);
+            throw new HttpException($e->getMessage(), 500);
         }
     }
 
